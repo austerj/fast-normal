fb36dd3a2eea0333f0973fb457052ae316116c03
austerj <johanauster@gmail.com>
2025-02-20 23:10:21 +0100

    ref: inline steps for readability

--- fastnorm/norm/fast.py
+++ fastnorm/norm/fast.py
@@ -12,30 +12,11 @@ from fastnorm import splitmix64
 from fastnorm.types import Vector
 
 
-def _invert_cdf(npartitions: int, qmax: float) -> np.ndarray:
+def _invert_cdf(npartitions: int, qmax: float) -> Vector[np.float64]:
     """Equidistant steps of inverse standard normal CDF (quantiles) from mid-point up to provided max."""
     if not (0.5 < qmax < 1.0):
         raise ValueError("Maximal quantile must be strictly between 0.5 and 1.0")
     return stats.norm.ppf([0.5 + i / (npartitions - 1) * (qmax - 0.5) for i in range(npartitions)])
-
-
-@nb.jit(nb.float64(nb.float64, nb.uint64))
-def _signed(f: float, n: int) -> float:
-    # multiplies the float by -1 if negative, otherwise by 1
-    is_negative = bool(n << (64 - 1))  # use bit [0] for sign
-    return (-is_negative + (not is_negative)) * f
-
-
-@nb.jit(nb.uint64(nb.uint64))
-def _int_bits(n: int) -> int:
-    # truncate to lowest 11 bits, then remove lowest bit (sign)
-    return (n << (64 - (10 + 1))) >> (64 - 10)
-
-
-@nb.jit(nb.float64(nb.uint64))
-def _uint_to_float(n: int) -> float:
-    # use highest 53 bits of unsigned int for float in [0, 1)
-    return (n >> 11) * 2 ** (-53)
 
 
 def _sampler_from_ints(npartitions: int, qmax: float):
@@ -45,15 +26,6 @@ def _sampler_from_ints(npartitions: int, qmax: float):
 
     table = _invert_cdf(npartitions + 1, qmax)
 
-    @nb.jit(nb.uint64(nb.uint64))
-    def _index(n: int) -> int:
-        # get index bits
-        idx = _int_bits(n)
-        # multiply by number of unique values...
-        idx *= npartitions
-        # ...and scale down to 0, 1, 2, ...
-        return idx >> 10
-
     # NOTE: taking uint64 array as input so this can be benchmarked independently of the bit generator
     @nb.jit(nb.float64[:](nb.uint64[:]), boundscheck=False, fastmath=True)
     def sample_from_ints(ints: Vector[np.uint64]) -> Vector[np.float64]:
@@ -62,13 +34,18 @@ def _sampler_from_ints(npartitions: int, qmax: float):
         z = np.empty(nsamples, dtype=np.float64)
         for i in range(nsamples):
             n = ints[i]
-            idx = _index(n)  # use bits [1:11] for index
-            f = _uint_to_float(n)  # use highest 53 bits [11:64] for uniform float in [0, 1)
-            # uniform sample within table segment
+            # truncate to lowest 11 bits, then remove lowest bit (sign)
+            idx = (n << (64 - (10 + 1))) >> (64 - 10)
+            idx *= npartitions  # multiply by number of unique values...
+            idx >>= 10  # ...then scale down to 0, 1, 2, ..., npartitions - 1
+            # use highest 53 bits of unsigned int for float in [0, 1)
+            f = (n >> 11) * 2 ** (-53)
+            # generate uniform sample of absolute value within table segment
             l, u = table[idx], table[idx + 1]
             abs_z = l + f * (u - l)
-            # retrieve sign from first bit of integer
-            z[i] = _signed(abs_z, n)
+            # retrieve sign from first bit
+            is_negative = bool(n << (64 - 1))
+            z[i] = (-is_negative + (not is_negative)) * abs_z  # equivalent: -abs_z if is_negative else abs_z
         return z
 
     return sample_from_ints
