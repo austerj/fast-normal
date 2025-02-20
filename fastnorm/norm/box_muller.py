0e7bc0b716ba266568c8e3cc8cf663a91ea6ee8f
austerj <johanauster@gmail.com>
2025-02-20 19:28:02 +0100

    ref: restructure box-muller sampling

--- fastnorm/norm/box_muller.py
+++ fastnorm/norm/box_muller.py
@@ -13,22 +13,26 @@ from fastnorm import splitmix64
 from fastnorm.types import Vector
 
 
-@nb.jit(nb.void(nb.float64[:]), boundscheck=False, fastmath=True)
-def _transform_array(array: np.ndarray) -> None:
-    """Transform array of uniform 64-bit floats to standard normal samples via Box-Muller transform."""
-    # assumes array length is divisible by 2
-    for i in range(0, array.shape[0], 2):
-        u1, u2 = array[i], array[i + 1]
+# NOTE: taking uint64 array as input so this can be benchmarked independently of the bit generator
+@nb.jit(nb.float64[:](nb.uint64[:]), boundscheck=False, fastmath=True)
+def _sample_from_ints(ints: Vector[np.uint64]) -> Vector[np.float64]:
+    """Sample an array of standard normal 64-bit floats from array of 64-bit unsigned integers."""
+    # assumes integer array length is divisible by 2
+    z = np.empty(ints.shape[0], dtype=np.float64)
+    for i in range(0, ints.shape[0], 2):
+        u1 = splitmix64.to_float(ints[i])
+        u2 = splitmix64.to_float(ints[i + 1])
         r = math.sqrt(-2 * math.log(u1))
         x = 2 * math.pi * u2
-        array[i] = r * math.cos(x)
-        array[i + 1] = r * math.sin(x)
+        z[i] = r * math.cos(x)
+        z[i + 1] = r * math.sin(x)
+    return z
 
 
 @nb.jit(nb.float64[:](nb.uint64, nb.uint64))
 def sample(nsamples: int, seed: int) -> Vector[np.float64]:
     """Sample an array of standard normal 64-bit floats."""
     _nsamples = 2 * math.ceil(nsamples / 2)  # generate extra sample if nsamples is not divisible by 2
-    array = splitmix64.sample_floats(_nsamples, seed)
-    _transform_array(array)
-    return array[:nsamples]  # drop last sample if nsamples is not divisible by 2
+    ints = splitmix64.sample_ints(_nsamples, seed)
+    samples = _sample_from_ints(ints)
+    return samples[:nsamples]  # drop last sample if nsamples is not divisible by 2
