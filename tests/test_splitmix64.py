3e7b822855c0a2432f282ba16ad7f1905be30e51
austerj <johanauster@gmail.com>
2025-02-20 17:35:19 +0100

    fix: provide seed in test

--- tests/test_splitmix64.py
+++ tests/test_splitmix64.py
@@ -10,6 +10,6 @@ MOMENTS = [1 / (i + 1) for i in range(10)]
 # particular this does not assess independence. this serves as more of an implementation test of a
 # method presumed to be reasonably sound.
 def test_moments():
-    samples = sample(50_000) / (2**64 - 1)
+    samples = sample(50_000, seed=0) / (2**64 - 1)
     for i, moment in enumerate(MOMENTS):
         assert np.mean(samples**i) == pytest.approx(moment, rel=1e-2)
