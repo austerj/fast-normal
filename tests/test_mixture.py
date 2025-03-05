6e7a7657280f430a48acdc7fdf7c0a5db51ae24b
austerj <johanauster@gmail.com>
2025-03-06 00:22:01 +0100

    test: quantiles in pseudocode

--- tests/test_mixture.py
+++ tests/test_mixture.py
@@ -1,7 +1,9 @@
+import itertools
 import math
 
 import numpy as np
 import pytest
+from scipy.special import erfinv
 
 from fastnorm.norm import box_muller, mixture
 
@@ -39,3 +41,28 @@ def test_mgf():
     qrestrict = 0.999
     bm_samples = restrict(box_muller.sample(nsamples, seed=0), qrestrict)
     assert estimate_mgf(restrict(samples, qrestrict), 2) == pytest.approx(estimate_mgf(bm_samples, 2), rel=1e-2)
+
+
+def test_pseudocode():
+    def quantiles_pseudocode(npartitions: int, q: float):
+        # compute and scale quantiles via combined iteration (analogous to pseudocode)
+        quantiles = np.zeros(npartitions + 1)
+        delta = (q - 0.5) / npartitions
+        c = 0.0
+        for i in range(1, npartitions + 1):
+            step = 0.5 + i * delta
+            quantiles[i] = math.sqrt(2) * erfinv(2 * step - 1)
+            c += quantiles[i] * (quantiles[i] + quantiles[i - 1]) + quantiles[i - 1] ** 2
+        c = math.sqrt(c / (3 * npartitions))
+        quantiles /= c
+        return quantiles
+
+    # check that implementation with comprehensions matches pseudocode in paper
+    ns = [1, 2, 3, 4]
+    qs = [0.6, 0.7, 0.9, 0.99]
+    for n, q in itertools.product(ns, qs):
+        npartitions = 2**n
+        # compute and scale quantiles via comprehensions
+        Q = mixture._invert_cdf(npartitions + 1, q)
+        Q /= math.sqrt(mixture._var(Q))
+        assert np.allclose(Q, quantiles_pseudocode(npartitions, q))
